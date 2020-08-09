cdd19d0714601124ae428c3cc19ee7b57ac55618
Katihun <kayele@mum.edu>
2020-08-09 00:22:10 -0700

    update circle

--- JQueryDrawCircle/circleScript.js
+++ JQueryDrawCircle/circleScript.js
@@ -1,36 +1,20 @@
-/*function draw(){
-    function init(){
-        var canvas = document.getElementsByTagName('canvas');
-        var c = canvas.getContext('2d');
-        c.fillRect(0,0,myCanvas.width, myCanvas.height);
-        var radius = 1;
-        var color = 0;
+$(function () {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+    var radius = 10;
 
-        function drawCircle(){
-            c.fillStyle = 'hsl(' + color++ + ', 100%, 50%';
-            c.beginPath();  
-            c.arc(500,500,radius, 0, 2*Math.PI,false);
-            c.fill();
-            radius +=0.5;
-            if(radiua < 500){
-                requestAnimationFrame(drawCircle);
-            }
+    function draw() {
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'green';
+        ctx.fill();
+        ctx.stroke();
+        radius += 1;
+        if (radius < 200) {
+            requestAnimationFrame(draw);
         }
-        drawCircle();
     }
-    window.addEventListener('load',init,false);
-}*/
-
-function draw() {
-    let c = document.getElementById("canvas");
-    let ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-    ctx.stroke();
-}
-
-function cleardrawing() {
-    let c = document.getElementById("canvas");
-    let ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, c.width, c.height);
-}
+    draw();
+});
