9da73d5d87d70f77623e48bc843cdbffbc43861a
Katihun <kayele@mum.edu>
2020-08-09 12:12:38 -0700

    update circle

--- JQueryDrawCircle/circleScript.js
+++ JQueryDrawCircle/circleScript.js
@@ -4,6 +4,9 @@ $(function () {
     var centerX = canvas.width / 2;
     var centerY = canvas.height / 2;
     var radius = 10;
+    var radiusGrowthRate = 2;
+    document.getElementById("txtr").value = radius;
+
 
     function draw() {
         ctx.beginPath();
@@ -11,10 +14,24 @@ $(function () {
         ctx.fillStyle = 'green';
         ctx.fill();
         ctx.stroke();
-        radius += 1;
-        if (radius < 200) {
-            requestAnimationFrame(draw);
-        }
+        radius += radiusGrowthRate;
     }
-    draw();
+    function remove() {
+        ctx.beginPath();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        document.getElementById("txtr2").value = radius;
+        document.getElementById("txtr3").value = radiusGrowthRate;
+    }
+
+    var r = setInterval(draw, 250);
+    $("#canvas").click(function () {
+        clearInterval(r);
+        remove();
+    });
+
+    $("#btn").click(function(){
+        r = setInterval(draw, 250);
+    });
+
+
 });
